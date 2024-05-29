c01b4c10502fdda0686b42c96a14c146322758b3
Elwira Z <elwiranoga@gmail.com>
2024-05-29 14:19:57 +0200

    Web page with hero and nav

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const navMobile = document.querySelector('.nav-mobile')
+const navDesktop = document.querySelector('.nav-desktop')
+const navBtn = document.querySelector('.burger-btn')
+const navContainer = document.querySelector('.nav-container')
+const logoImg = document.querySelector('.logo__img')
+
+const handleNav = () => {
+	navBtn.classList.toggle('is-active')
+	navMobile.classList.toggle('nav-mobile--active')
+}
+
+// function addShadow() {
+// 	if (window.scrollY >= 100) {
+// 		navContainer.classList.add('shadow-bg')
+// 		logoImg.classList.add('is-transparent')
+// 	} else {
+// 		navContainer.classList.remove('shadow-bg')
+// 		logoImg.classList.remove('is-transparent')
+// 	}
+// }
+
+// window.addEventListener('scroll', addShadow)
+navBtn.addEventListener('click', handleNav)
