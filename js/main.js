6514a98d187eda12a1498ef65b3637160b81c248
Elwira Z <elwiranoga@gmail.com>
2024-06-04 18:08:37 +0200

    Adjusted media queries on home, added contact subsite, added footer

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const navDesktop = document.querySelector('.nav-desktop')
 const navBtn = document.querySelector('.burger-btn')
 const navContainer = document.querySelector('.nav-container')
 const logoImg = document.querySelector('.logo__img')
+const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
 	navBtn.classList.toggle('is-active')
@@ -12,12 +13,21 @@ const handleNav = () => {
 function addShadow() {
 	if (window.scrollY >= 100) {
 		navContainer.classList.add('shadow-bg')
+		navMobile.classList.add('shadow-bg')
 		logoImg.classList.add('is-transparent')
 	} else {
 		navContainer.classList.remove('shadow-bg')
+		navMobile.classList.remove('shadow-bg')
 		logoImg.classList.remove('is-transparent')
 	}
 }
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+handleCurrentYear()
 
 window.addEventListener('scroll', addShadow)
 navBtn.addEventListener('click', handleNav)
